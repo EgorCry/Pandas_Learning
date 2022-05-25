338700051c0489b36f3d26b58d7e928321ea0377
egorcry <abulapin@mail.ru>
2022-05-25 23:49:06 +0300

    Till Ex 2-9

---
+++ Exercises/Ex 2-7.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+
+account_info = pd.DataFrame({
+    'name': ['Bob', 'Mary', 'Mita'],
+    'account': [123846, 123972, 347209],
+    'balance': [123, 3972, 7209]
+})
+
+print(account_info.loc[1, 'balance'])
+print()
+print(account_info.loc[:, ['name', 'balance']])
