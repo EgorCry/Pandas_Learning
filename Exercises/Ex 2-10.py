612ef4943b44020fb22b1a14828d1f3e939f2e88
egorcry <abulapin@mail.ru>
2022-05-27 00:38:27 +0300

    Till 2-10

---
+++ Exercises/Ex 2-10.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+building_records_1844 = pd.DataFrame([['Grande Hotel', 1830],
+                                      ["Jone's Farm", 1842],
+                                      ['Public Library', 1836],
+                                      ['Marietta House', 1823]],
+                                     columns=['building', 'established']).set_index(['building'])
+
+print(building_records_1844)
+
+building_records_2020 = pd.DataFrame([["Sam's Bakery", 1962], ['Grande Hotel', 1830], ['Public Library', 1836],
+                                      ["Mayberry's Factory", 1924]],
+                                     columns=['building', 'established']).set_index(['building'])
+
+print()
+print(building_records_2020)
+
+cols = building_records_2020.columns.difference(building_records_1844.columns)
+
+print()
+print(pd.merge(
+    building_records_1844,
+    building_records_2020[cols],
+    how='inner',
+    on=['building']
+))
